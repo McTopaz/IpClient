d0e7f4938f9f157f4ecacb544644265f02051b75
Stefan Eriksson <stefan.k.j.eriksson@gmail.com>
2020-01-27 13:40:16 +0100

    Added color indication if request filed is not a all in hex.

--- IpClient/IpClient/ViewModels/vmMainPage.cs
+++ IpClient/IpClient/ViewModels/vmMainPage.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 using System.Net;
 
+using Xamarin.Forms;
+
 using IpClient.Misc;
+using IpClient.Clients;
 
 using PropertyChanged;
 
@@ -12,24 +16,38 @@ namespace IpClient.ViewModels
     [AddINotifyPropertyChangedInterface]
     class vmMainPage
     {
+        public const string UDP = "UDP";
+        public const string TCP = "TCP";
+
         public IPAddress IpAddress { get; set; } = IPAddress.Broadcast;
         public ushort Port { get; set; } = 10001;
-        public string Protocol { get; set; } = "UDP";
+        public string Protocol { get; set; } = UDP;
         public uint Timeout { get; set; } = 3000;
-        public IEnumerable<string> Protocols { get; private set; } = new string[] { "UDP", "TCP" };
-        public IEnumerable<byte> Request { get; set; } = new byte[] { 0xDE, 0xAD, 0xBE, 0xEF };
-        public IEnumerable<byte> Response = new List<byte>();
+        public IEnumerable<string> Protocols { get; private set; } = new string[] { UDP, TCP };
+        public string Request { get; set; } = "DEADBEEF";
+        public Color RequestColor { get; set; }
+        public string Response { get; set; }
 
-        public RelayCommand Send { get; private set; } = new RelayCommand();
+        public RelayCommand RequestChanged { get; private set; } = new RelayCommand();
+        public RelayCommand SendAndReceive { get; private set; } = new RelayCommand();
 
         public vmMainPage()
         {
-            Send.Callback += Send_Callback;
+            RequestChanged.Callback += RequestChanged_Callback;
+            SendAndReceive.Callback += SendAndReceive_Callback;
         }
 
-        private void Send_Callback(object parameter = null)
+        private void RequestChanged_Callback(object parameter = null)
         {
-            
+            var allHex = Enumerable.Range(0, Request.Length)
+                .Select(i => Request[i])
+                .All(c => Uri.IsHexDigit(c));
+            RequestColor = allHex ? Color.FromRgba(0, 0, 0, 0) : Color.Red;
+        }
+
+        private void SendAndReceive_Callback(object parameter = null)
+        {
+            Response = ClientHandler.SendAndReceive(IpAddress, Port, Protocol, Request);
         }
     }
 }
